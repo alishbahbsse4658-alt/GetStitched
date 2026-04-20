8aadd0363b64ec7420357ad304b64f2f37a33e6c
Lishayy <yourgithubemail@gmail.com>
2026-04-20 22:51:00 +0500

    fix navbar path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
