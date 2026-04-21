2bb89d0c1ecc4023279933422430ac1465b26cd6
Lishayy <yourgithubemail@gmail.com>
2026-04-21 10:09:30 +0500

    fix: capital C in Components import path

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
+import NavBar from './Components/NavBar';
+import Footer from './Components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import TailorList from './pages/TailorList';
@@ -11,7 +11,7 @@ import RegisterTailor from './pages/RegisterTailor.jsx';
 import RegisterCustomer from './pages/RegisterCustomer';
 import Booking from './pages/Booking';
 import ComingSoon from './pages/ComingSoon'; 
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './Components/ScrollToTop';
 import './App.css';
 
 function App() {
